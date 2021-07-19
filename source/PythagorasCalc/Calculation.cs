2d16d027809c69acfc245e2543cde0dba8c87590
jacobdrugan <Jacobdrugan@gmail.com>
2021-07-19 16:39:09 +0100

    Commit - 19072021
    Added more to pythagoras.

---
+++ source/PythagorasCalc/Calculation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+
+namespace PythagorasCalc
+{
+    public class tricalculation
+    {
+
+        public static void calculations()
+        {
+            Console.WriteLine("Hello World!");
+            SolidBrush sb = new SolidBrush(Color.Black) 
+
+        }
+    }
+}
--- source/PythagorasCalc/Program.cs
+++ source/PythagorasCalc/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PythagorasCalc
 {
@@ -6,7 +7,7 @@ namespace PythagorasCalc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            tricalculation.calculations();
         }
     }
 }

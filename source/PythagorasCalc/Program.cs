398e00fdbe3223c22699a2b6b963d621f8eb5308
jacobdrugan <Jacobdrugan@gmail.com>
2021-07-19 16:05:48 +0100

    Commit - 19072021
    Added new calc app.

---
+++ source/PythagorasCalc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PythagorasCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

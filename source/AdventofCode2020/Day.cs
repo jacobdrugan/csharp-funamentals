7bac79aee11bc15c0d82b3aeda371b053e514e1f
jacobdrugan <Jacobdrugan@gmail.com>
2021-07-20 20:12:53 +0100

    Starting the Advent of Code 2020

---
+++ source/AdventofCode2020/Day.cs
@@ -0,0 +1,19 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventofCode2020
+{
+    public class Day1
+    {
+        public static void maintask()
+        {
+            List<string> puzinp;
+            string filepath = "day1.txt";
+            puzinp = File.ReadAllLines(filepath).ToList();
+
+           
+        }
+    }
+}
---
+++ source/AdventofCode2020/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AdventofCode2020
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Day1.maintask();
+            Console.ReadKey();
+        }
+    }
+}

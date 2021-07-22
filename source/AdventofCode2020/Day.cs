87a854e16a0d99d4139d044cb454633c04cff3f2
jacobdrugan <Jacobdrugan@gmail.com>
2021-07-22 17:30:06 +0100

    Added more to Day 1 of AOC

--- source/AdventofCode2020/Day.cs
+++ source/AdventofCode2020/Day.cs
@@ -11,27 +11,71 @@ namespace AdventofCode2020
         {
             string filepath = "day1.txt";
             List<int> puzinp = File.ReadAllLines(filepath).Select(Int32.Parse).ToList();
-            //var target = 2020;
+            List<int> puzinplow;
+            puzinplow = new List<int>();
+            List<int> puzinphigh;
+            puzinphigh = new List<int>();
+            int med = puzinp.Count()/2;
+            int b = 0;
+            int d = 0;
+            int val0 = 0; 
+            int val1 = 0;
+            int val2 = 0;
+            int target = 2020;
+
             //Order list
             puzinp.Sort();
-            foreach (int a in puzinp)
-            {
-                Console.WriteLine($"{a}");
-            }
-            //Find Median
-            int med = puzinp.Count()/2;
+
             //Split at median
             foreach(int a in puzinp)
             {
-                if(puzinp[a] > med)
-                    {
-                    Console.WriteLine(a);
-                    }
+                if (puzinp[b] > puzinp[med])
+                {
+                    puzinphigh.Add(a);
+                    b++;
+                }
                 else
-                    {
-                    Console.WriteLine("BELOW");
-                    }
+                {
+                    puzinplow.Add(a);
+                    b++;
+                }
             }
+            foreach(int listitem in puzinphigh)
+            {
+                val0 = target-listitem;
+                if(val0 = puzinplow[d])
+                {
+                    
+                }
+                else
+                {
+
+                }
+            }
+            // foreach(int c in puzinphigh)
+            // {
+            //     Console.WriteLine("HERE");
+            //     val0 = (2020 - c);
+            //     if(val0 == puzinplow[d])
+            //     {
+            //         val2 = puzinplow[d];
+            //         val1 = puzinphigh[c];
+            //     }
+            //     else
+            //     {
+            //         d++;
+            //     }
+            // }
+            // Console.WriteLine($"High value = {val1}");
+            // Console.WriteLine($"Low value = {val2}");
+            // foreach (int c in puzinphigh)
+            // {
+            //     Console.WriteLine($"HighList:{c}");       
+            // }
+            // foreach (int c in puzinplow)
+            // {
+            //     Console.WriteLine($"LowList:{c}");
+            // }
             //take top away from 2020 and see if small matches result 2020 - x smaller than x  
             
         }
